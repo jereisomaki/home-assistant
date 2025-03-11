0f932b1cd2f11692e33de624aa2db4554f886400
Jere Isomäki <jere.isomaki@tridea.fi>
2025-03-11 17:11:09 +0200

    Remove unused pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-import { useState } from "react";
-import { useSwipeable } from "react-swipeable";
 import { useTheme } from "./hooks/useTheme";
 import { useLanguage } from "./hooks/useLanguage";
 
 import { LANGUAGE_OPTIONS } from "./constants";
 
 import ElectricityPriceData from "./pages/electricityPriceData";
-import WeatherData from "./pages/weatherData";
 
 import Clock from "./components/Clock";
 import Select from "./components/Select";
-import ThemeController from "./components/ThemeController";
+import ThemeController from "./components/Button/ThemeButton";
 
 const App = () => {
-  const [currentComponent, setCurrentComponent] = useState("electricity");
-
   const { theme, toggleTheme } = useTheme();
   const { language, setLanguage, t } = useLanguage("en");
-
-  const swipeHandler = (e) => {
-    if (e.dir === "Left") setCurrentComponent("weather");
-    else if (e.dir === "Right") setCurrentComponent("electricity");
-  };
 
   const handleLanguage = (e) => {
     setLanguage(e.target.value);
   };
 
-  const handlers = useSwipeable({
-    onSwiped: swipeHandler,
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
-  });
-
   return (
-    <div className={`flex h-screen items-center justify-center bg-background-600`} {...handlers}>
+    <div className={`flex h-screen items-center justify-center bg-background-600`}>
       <div className="flex flex-col flex-1 h-full">
         <div className="flex justify-between items-center px-4 pt-3">
           <Clock />
           <div className={"flex gap-4"}>
             <Select value={language} options={LANGUAGE_OPTIONS} onChange={handleLanguage} />
-            <ThemeController theme={theme} onChange={toggleTheme} />
+            <ThemeController checked={theme === "light"} onChange={toggleTheme} />
           </div>
         </div>
-        {currentComponent === "electricity" ? <ElectricityPriceData t={t} /> : <WeatherData t={t} />}
+        <ElectricityPriceData t={t} />
       </div>
     </div>
   );
--- src/pages/weatherData.jsx
+++
@@ -1,5 +0,0 @@
-const WeatherData = () => {
-  return <div className="flex flex-col gap-4 w-full h-full p-4">asd</div>;
-};
-
-export default WeatherData;
